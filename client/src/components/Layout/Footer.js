52629ce32ccd8be9b7c172d29ff16eea16b0ab94
“Jayani <jayaniabhiram@gmail.com>
2024-11-17 23:27:50 +0530

    change

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white py-8">
+    <div className="container mx-auto flex justify-center">
+      
+    </div>
+  </footer>
+  )
+}
+
+export default Footer
